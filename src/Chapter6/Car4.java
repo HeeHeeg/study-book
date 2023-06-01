11481eda5adab0556cf6e85e1b74bcb7283d3bfb
HeeHeeg <qmffndoden@naver.com>
2023-06-01 20:48:00 +0900

    생성자

--- src/Chapter6/Car4.java
+++ src/Chapter6/Car4.java
@@ -1,3 +1,4 @@
+/*
 package Chapter6;
 
 public class Car4 {
@@ -28,3 +29,4 @@ public class Car4 {
         this.maxspeed = maxspeed;
     }
 }
+*/
--- src/Chapter6/Car4_1Example.java
+++ src/Chapter6/Car4_1Example.java
@@ -1,23 +1,22 @@
-/*
 package Chapter6;
 
-public class Car4Example {
+public class Car4_1Example {
     public static void main(String[] agrs) {
-        Car4 car1 = new Car4();
+        Car4_1 car1 = new Car4_1();
         System.out.println("car1.company: " + car1.company);
 
-        Car4 car2 = new Car4("자가용");
+        Car4_1 car2 = new Car4_1("자가용");
         System.out.println("car2.company: " + car2.company);
         System.out.println("car2.model: " + car2.model);
 
-        Car4 car3 = new Car4("택시", "빨강");
+        Car4_1 car3 = new Car4_1("택시", "빨강");
         System.out.println("car3.company: " + car3.company);
         System.out.println("car3.model: " + car3.model);
         System.out.println("car3.color: " + car3.color);
 
 
 
-        Car4 car4 = new Car4("버스", "파랑", 300);
+        Car4_1 car4 = new Car4_1("버스", "파랑", 300);
         System.out.println("car4.company: " + car4.company);
         System.out.println("car4.model: " + car4.model);
         System.out.println("car4.color: " + car4.color);
@@ -29,4 +28,3 @@ public class Car4Example {
 
     }
 }
-*/
