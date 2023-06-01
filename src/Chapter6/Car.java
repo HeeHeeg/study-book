b378a1a0e0f05f7805a6df1b4dc8e57befcb9569
HeeHeeg <qmffndoden@naver.com>
2023-06-01 09:59:42 +0900

    Chapter6

---
+++ src/Chapter6/Car.java
@@ -0,0 +1,6 @@
+package Chapter6;
+
+public class Car {
+    Car(String color, int cc) {
+    }
+}
---
+++ src/Chapter6/CarExample.java
@@ -0,0 +1,10 @@
+package Chapter6;
+
+public class CarExample {
+    public static void main(String[] args) {
+        Car car = new Car("Black", 3000);
+        System.out.println(car);
+
+    }
+
+}
