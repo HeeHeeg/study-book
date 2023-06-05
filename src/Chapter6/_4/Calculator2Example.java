b3b19031c415ea36819bf2f7ce898cc539d5f537
HeeHeeg <qmffndoden@naver.com>
2023-06-05 16:37:52 +0900

    혼공자 손코딩6/5

---
+++ src/Chapter6/_4/Calculator2Example.java
@@ -0,0 +1,17 @@
+package Chapter6._4;
+
+public class Calculator2Example {
+    public static void main(String[] args) {
+        Calculator2 calcu2 = new Calculator2();
+
+        //정사각형 넓이 구하기
+        double result1 = calcu2.areaRectangle(10);
+
+        //직사각형 넓이 구하기
+        double result2 = calcu2.areaRectangle(10, 20);
+
+        //결과 출력
+        System.out.println("정사각형 넓이 =" + result1);
+        System.out.println("직사학경 넓이 =" + result2);
+    }
+}
---
+++ src/Chapter6/_4/Car2Example.java
@@ -0,0 +1,16 @@
+package Chapter6._4;
+
+public class Car2Example {
+    public static void main(String[] args) {
+        Car2 mycar = new Car2();
+        mycar.keyTurnOn();
+        mycar.run();
+        //내가 해본것. 내가해본것과 아래의 차이점은?
+//        mycar.getSpeed();
+//        System.out.println("현재 속도: " + mycar.speed + "km/h");
+
+        int speed = mycar.getSpeed();
+        System.out.println("현재 속도: " + speed + "km/h");
+
+    }
+}
